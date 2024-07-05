9d60f2d9031aa70399a659eb087e8a88f3210b48
Munkhod1 <munkhoddl@gmail.com>
2024-07-05 10:15:02 +0800

    july 5

---
+++ todo.js
@@ -0,0 +1,14 @@
+const todoListEl = document.getElementById("taskTodoList");
+const doneListEl = document.getElementById("doneList");
+const progressListEl = document.getElementById("progressList");
+const blockedListEl = document.getElementById("blockedList");
+//
+const myModal = document.getElementById("myModal");
+const myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", () => {
+  myInput.focus();
+});
+//
+
+function zurah() {}
